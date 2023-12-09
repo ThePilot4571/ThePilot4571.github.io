4c5d865c2d0bc38d35b46f83b8714565387e87f1
ThePilot4571 <boraayhan3737@gmail.com>
2023-12-08 23:39:18 -0700

    Added automatic greeting based on local time

---
+++ main.js
@@ -0,0 +1,7 @@
+now = new Date(),
+    now.getTimezoneOffset() * 60 * 1000;
+g = document.getElementById('greeting');
+g.innerText = "";
+if(now.getHours() < 12) g.innerText = "Good morning.";
+else if(now.getHours() < 17) g.innerText = "Good afternoon.";
+else g.innerText = "Good evening.";
